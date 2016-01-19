19415e3f799b7e6264a34611de4a776b75fd8586
Ben Ricker <BGRicker@gmail.com>
2016-01-18 21:55:56 -0500

    stock partial

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   #root 'welcome#index'
   root 'users#my_portfolio'
   get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
 end
